bcb71130f93d74242bc841f7a58a9bed8a35f92b
Edgar <edgar135300@gmail.com>
2019-10-09 23:22:35 -0700

    Rearranged and rorganized the code. Made the StringBuffer history private.

--- WorkSpace/src/cse360assign2/AddingMachine.java
+++ WorkSpace/src/cse360assign2/AddingMachine.java
@@ -1,39 +1,42 @@
 /*Brian Farias
  * CSE360-85141
  * Assignment 2
- * AddingMachine 
+ * AddingMachine
  * Git practice
  */
 
 package cse360assign2;
 
 public class AddingMachine {
-	
+
 	private int total;
-	StringBuffer history;
-	
+	private StringBuffer history;
+
 	public AddingMachine () {
-		total = 0;  
+		total = 0;
 		history = new StringBuffer("0");
-		}
+	}
+
 	public int getTotal () {
 		return total;
-		}
+	}
+
 	public void add (int value) {
-		total = total + value; //adds 
+		total = total + value; //adds
 		history.append(" + " + value);
-		}
+	}
+
 	public void subtract (int value) {
-		total = total - value;
+		total = total - value; //subtracts
 		history.append(" - " + value);
-		}
-	public String toString () {
-		return history.toString();
-		
-		}
+	}
+
 	public void clear() {
 		total = 0;
 	}
-	
-}
 
+	public String toString () {
+		return history.toString();
+
+	}
+}
