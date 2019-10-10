3577843ecf9d32df6d1013517f7aa4298f7b82eb
Brian <bfarias1@asu.edu>
2019-10-09 20:42:00 -0700

    Test Push

---
+++ WorkSpace/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,29 @@
+/*Brian Farias
+ * CSE360-85141
+ * Assignment 2
+ * AddingMachine 
+ * Git practice
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+	private int total;
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+		}
+	public int getTotal () {
+		return 0;
+		}
+	public void add (int value) {
+		
+		}
+	public void subtract (int value) {
+		
+		}
+	public String toString () {return "";}public void clear() {
+		
+	}
+	
+}
+
